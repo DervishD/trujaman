65542f70d41d012bb5d7deeccc9c823ad2e456c2
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2021-02-10 17:58:18 +0100

    Add status indicators to main page
    In order to help debugging the PWA, specially the service worker part
    which is a bit tricky and complex, some status indicators are added to
    the main page.
    
    For now, only indicators about the service worker are present, but in
    the future the same framework may be used for other indicators. Also the
    entire indicator area can be hidden in production, if needed, it's all
    in a single <div>.
    
    The changes in the service worker code are not exactly needed for this,
    but they have been added to this commit because they were needed for
    testing the indicator system, so it makes sense to commit them now.

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 "use strict";
 
-let trujaman_version = '0.0.1-alpha';
+let trujaman_version = '0.0.2-alpha';
+
+// Show current version on page.
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('version').textContent = 'version ' + trujaman_version;
+});
+
+function set_status (status, value) {
+    document.getElementById(status).textContent = value ? ' YES' : ' NO';
+}
 
 // Register service worker.
-if ('serviceWorker' in navigator) {                                 
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
+        set_status('sw_supported', true);  // Indicate service worker (PWA) support.
+
+        // For now, set a flag to show whether this page is being controlled or not.
+        set_status('page_controlled', navigator.serviceWorker.controller);
+        navigator.serviceWorker.addEventListener('controllerchange', event => {
+            set_status('page_controlled', true);
+        });
+
+        // For now, set a flag to show that the PWA is, in fact, installable.
+        set_status('pwa_installable', false);  // By default...
+        window.addEventListener('beforeinstallprompt', event => {
+            // Prevent the default install handler to appear for now.
+            event.preventDefault();
+            set_status('pwa_installable', true);
+        });
+
         navigator.serviceWorker.register('sw.js').then(registration => {
-            console.debug('Service worker registered.', registration);
+            set_status('sw_registered', true);
+
+            // This is a starting point, to show the status after page load.
+            set_status('sw_active', registration.active);
+            set_status('sw_waiting', registration.waiting);
+            set_status('sw_installing', registration.installing);
+
+            // Handle state changes for new service workers, including the first one.
+            registration.addEventListener('updatefound', () => {
+                set_status('sw_installing', true);
+                registration.installing.onstatechange = event => {
+                    if (event.target.state == 'installed' || event.target.state == 'activated') {
+                        set_status('sw_active', registration.active);
+                        set_status('sw_waiting', registration.waiting);
+                        set_status('sw_installing', registration.installing);
+                    }
+                }
+            });
         }).catch(error => {
-            console.error('Service worker registration failed.');
-            console.error(error);
+            set_status('sw_registered', false);
+            console.error('Service worker registration failed:', error);
         });
     });
 } else {
-    console.warn('Browser is not compatible with Progressive Web Apps.');
+    // Indicate that there's no service worker (PWA) support.
+    window.addEventListener('load', () => set_status('sw_supported', false));
 }
--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
 self.addEventListener('install', async event => {
     console.debug('Installing service worker.');
+    event.waitUntil(self.skipWaiting());  // Brutal, but effective for now.
 });
-
 
 self.addEventListener('activate', event => {
     console.debug('New service worker activated!');
+    event.waitUntil(self.clients.claim());  // Brutal, but effective for now.
 });
+
+self.addEventListener('fetch', () => {});  // Just a placeholder for now.

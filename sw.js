92618c25c20c68be7bc177e77b48f2e466c312d1
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2021-02-13 17:23:50 +0100

    Precache assets

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
-self.addEventListener('install', async event => {
+const trujaman = {
+    staticCache: 'trujaman-v0',
+}
+
+// Precache assets and take control (for now)
+self.addEventListener('install', event => {
     console.debug('Installing service worker.');
-    event.waitUntil(self.skipWaiting());  // Brutal, but effective for now.
+    self.skipWaiting();  // Brutal, but effective for now.
+    event.waitUntil(
+        caches.open(trujaman.staticCache).then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/index.css',
+                '/index.js',
+                '/manifest.json',
+                '/appicon.png',
+                '/favicon.ico',
+          ]);
+        }),
+    );
 });
 
 self.addEventListener('activate', event => {
@@ -13,5 +31,13 @@ self.addEventListener('activate', event => {
 // This is ABSOLUTELY discouraged, for a whole variety of reasons, but here it's just used as a starting point.
 self.addEventListener('fetch', event => {
     console.debug('Fetching', event.request.url);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.open(trujaman.staticCache).then(cache =>
+            cache.match(event.request).then(response => {
+                // For now, just detect uncached assets, but always fetch from network.
+                if (!response) console.error('UNCACHED request for', event.request.url);
+                return fetch(event.request);
+            })
+        )
+    );
 });

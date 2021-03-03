a0bcfa418718c057e517468219382a7c87921dd6
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2021-03-03 17:08:07 +0100

    Refactor some code

--- index.js
+++ index.js
@@ -54,89 +54,85 @@ window.addEventListener('load', () => {
     // Show logging console during development.
     if (DEBUG) document.querySelector('div#trujaman_stdlog').hidden = false;
 
-    if (window.FileReader) {
-        // Show all needed page elements.
-        document.querySelector('div#trujaman_fileloader').hidden = false;
-        trujamanLog('HTML5 File API is supported.');
-        trujamanSay('OK');
-    } else {
+    // Feature detection
+    if (!window.FileReader)  // HTML5 File API.
         trujamanDie('Lo siento, trujamán no funcionará porque el navegador no es compatible con File API.');
-    }
-});
 
+    if (!'serviceWorker' in navigator)   // Service workers (PWA support).
+        trujamanDie('Service workers are NOT supported.')
 
-// Register service worker.
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        trujamanLog('Service workers are supported.');
+    trujamanLog('HTML5 File API is supported.');
+    trujamanLog('Service workers are supported.');
+    trujamanLog(`The page has${navigator.serviceWorker.controller?'':' not'} a controller.`);
 
-        // Indicate wether the page is currently controlled or not.
-        trujamanLog(`The page has${navigator.serviceWorker.controller?'':' not'} a controller.`);
+    // Show all needed page elements.
+    document.querySelector('div#trujaman_fileloader').hidden = false;
 
-        navigator.serviceWorker.ready
-        .then(registration => {
-            fetch('version')
-            .then(response => response.text())
-            .then(version => document.getElementById('trujaman_version').textContent = 'v' + version);
-        });
-
-        let refreshing = false;
-        navigator.serviceWorker.addEventListener('controllerchange', event => {
-            trujamanLog('The page has a new controller.');
-            if (refreshing) return;
-            refreshing = true;
-            trujamanSleep(1000).then(() => window.location.reload());  // Debugging only, FIXME!
-        });
-
-        window.addEventListener('beforeinstallprompt', event => {
-            event.preventDefault();  // Prevent the default install handler to appear for now.
-            // Indicate the PWA is installable.
-            trujamanLog('PWA trujamán seems installable.');
-        });
-
-        navigator.serviceWorker.register('sw.js')
-        .then(registration => {
-            trujamanLog('Successful service worker registration.')
-
-            trujamanLog(`There is${registration.active?'':' not'} an active service worker.`);
-
-            if (registration.waiting) trujamanLog('There is a service worker waiting.');
-
-            // Handle state changes for new service workers, including the first one.
-            registration.addEventListener('updatefound', () => {
-                trujamanLog('Updated service worker found.');
-                registration.installing.onstatechange = event => {
-                    if (event.target.state == 'installed')
-                        trujamanLog('New service worker installed.');
-                    if (event.target.state == 'activated')
-                        trujamanLog('New active service worker.');
-                }
-            });
-        })
-        .catch(error => {
-            trujamanLog('Service worker registration failed with', error);  // Only for debugging, FIXME!
-            console.error('Service worker registration failed with', error);
-        });
-
-        let theInput = document.getElementsByTagName('input')[0];
-        let theBrowseButton = document.querySelector('button#trujaman_browse');
-        let theConvertButton = document.querySelector('button#trujaman_convert');
-
-        theInput.addEventListener('change', event => {
-            theConvertButton.disabled = false;
-            theConvertButton.firstElementChild.innerText = `«${event.target.files[0].name}»`;
-            theConvertButton.focus();
-        });
-
-        theBrowseButton.addEventListener('click', event => {
-            theInput.click(event);
-        });
-
-        theConvertButton.addEventListener('click', event => {
-            console.log('Must convert, aaagh.');
-        });
+    // Show version number.
+    navigator.serviceWorker.ready
+    .then(registration => {
+        fetch('version')
+        .then(response => response.text())
+        .then(version => document.getElementById('trujaman_version').textContent = 'v' + version);
     });
-} else {
-    // Indicate that there's no service worker (PWA) support.
-    window.addEventListener('load', () => trujamanLog('Service workers are NOT supported.'));
-}
+
+    // Handle controller change.
+    let refreshing = false;
+    navigator.serviceWorker.addEventListener('controllerchange', event => {
+        trujamanLog('The page has a new controller.');
+        if (refreshing) return;
+        refreshing = true;
+        trujamanSleep(1000).then(() => window.location.reload());  // Debugging only, FIXME!
+    });
+
+    // Handle PWA installation offers.
+    window.addEventListener('beforeinstallprompt', event => {
+        event.preventDefault();  // Prevent the default install handler to appear for now.
+        // Indicate the PWA is installable.
+        trujamanLog('PWA trujamán seems installable.');
+    });
+
+    // Register service worker.
+    navigator.serviceWorker.register('sw.js')
+    .then(registration => {
+        trujamanLog('Successful service worker registration.')
+
+        trujamanLog(`There is${registration.active?'':' not'} an active service worker.`);
+
+        if (registration.waiting) trujamanLog('There is a service worker waiting.');
+
+        // Handle state changes for new service workers, including the first one.
+        registration.addEventListener('updatefound', () => {
+            trujamanLog('Updated service worker found.');
+            registration.installing.onstatechange = event => {
+                if (event.target.state == 'installed')
+                    trujamanLog('New service worker installed.');
+                if (event.target.state == 'activated')
+                    trujamanLog('New active service worker.');
+            }
+        });
+    })
+    .catch(error => {
+        trujamanLog('Service worker registration failed with', error);  // Only for debugging, FIXME!
+        console.error('Service worker registration failed with', error);
+    });
+
+    // Set up the file loader.
+    let theInput = document.getElementsByTagName('input')[0];
+    let theBrowseButton = document.querySelector('button#trujaman_browse');
+    let theConvertButton = document.querySelector('button#trujaman_convert');
+
+    theInput.addEventListener('change', event => {
+        theConvertButton.disabled = false;
+        theConvertButton.firstElementChild.innerText = `«${event.target.files[0].name}»`;
+        theConvertButton.focus();
+    });
+
+    theBrowseButton.addEventListener('click', event => {
+        theInput.click(event);
+    });
+
+    theConvertButton.addEventListener('click', event => {
+        console.log('Must convert, aaagh.');
+    });
+});

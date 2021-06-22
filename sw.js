008bbbe9858a9acb702d488916247f084857ac57
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2021-06-22 15:38:26 +0200

    Fix linting problems in service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const serviceworkerVersion = '20210617' + '-alpha';
+const serviceworkerVersion = '20210622-alpha';
 
 const landingPage = '.';  // Maybe: "new URL(self.registration.scope).pathname"???
 
@@ -22,13 +22,12 @@ const coreAssets = [
     'ww.js',
     'manifest.json',
     'appicon.png',
-    'favicon.ico',
+    'favicon.ico'
 ];
 
 
 // Precache assets and take control (for now)
 self.addEventListener('install', event => {
-    console.debug('Installing service worker.');
     event.waitUntil(
         caches.open(coreCacheName)
         .then(cache => cache.addAll(coreAssets))
@@ -39,14 +38,14 @@ self.addEventListener('install', event => {
 
 // Delete old caches and take control of uncontrolled pages.
 self.addEventListener('activate', event => {
-    console.debug('New service worker activated!');
     event.waitUntil(
         caches.keys()
-        .then(keys => {
-            // Only old caches from this PWA are deleted. Check the prefix!
-            keys = keys.filter(key => key.startsWith(cachePrefix)).filter(key => key != coreCacheName);
-            return Promise.all(keys.map(key => caches.delete(key)));
-        })
+        .then(keys => Promise.all(
+            keys
+            .filter(key => key.startsWith(cachePrefix))
+            .filter(key => key !== coreCacheName)  // Only old caches from this PWA are deleted. Check the prefix!
+            .map(key => caches.delete(key))
+        ))
         .then(self.clients.claim())  // Brutal, but effective for now.
     );
 });
@@ -55,24 +54,27 @@ self.addEventListener('activate', event => {
 // A 'cache-only' caching strategy is used for now.
 // This makes sure the PWA fully works when offline, and it's perfect for the core assets.
 self.addEventListener('fetch', event => {
-    if (event.request.method != 'GET') {
+    if (event.request.method !== 'GET') {
+        // eslint-disable-next-line no-console
         console.error('Fetch with non-GET method!');  // Should NEVER happen in production.
         return;
     }
     if (!event.request.url.startsWith(self.location.origin)) {
+        // eslint-disable-next-line no-console
         console.error('Cross-origin fetch!');  // Should NEVER happen in production.
         return;
     }
+    // eslint-disable-next-line no-console
     console.debug('Fetching', event.request.url);
-    event.respondWith(function () {
+    event.respondWith((() => {
         if (event.request.url.endsWith('version')) return new Response(serviceworkerVersion);
 
         // This is TEMPORARY!
         // This is needed to be able to test changes fast and at the same time having offline functionality.
         return fetch(event.request).catch(async () => {
-            let cache = await caches.open(coreCacheName);
-            let response = await cache.match(event.request);
+            const cache = await caches.open(coreCacheName);
+            const response = await cache.match(event.request);
             return response || cache.match(landingPage);
         });
-    }());
+    })());
 });

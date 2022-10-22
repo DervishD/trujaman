ed3d97b45c78c02cbf9a7dec1cdc9231e06b13eb
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2022-10-22 15:08:43 +0200

    Sanitize repository
    For a start, cSpell ignored words are handled now in a different way, by
    adding them as comments with directives for the spell checker (cSpell in
    this case), instead of using the IDE local settings file.
    
    Also, the 'zhttpd.cmd' file, containing the command to run a local HTTP
    server for development and debugging purposes, has been removed: it has
    nothing to do with the repository itself, but rather with a particular
    development environment. It is better to remove it.

--- zhttpd.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-title Web server
-rem Use Node.js http-server.
-rem Options: don't cache, don't display directory listings, open browser window automatically after start.
-http-server -c-1 -d false -o

155e6e620d16944d5af58fce70f9ee6313daea88
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2023-04-22 08:58:22 +0200

    Improve comments in version.js
    Some redundant comments have been removed and some code adapted to make,
    in fact, those comments redundant.

--- version.js
+++ version.js
@@ -2,7 +2,6 @@
 
 /* exported version DEBUG */
 
-// Version of the PWA.
 // Follows Semantic Versioning 2.0.0 (https://semver.org/spec/v2.0.0.html).
 const version = {
     'major': '0',
@@ -15,6 +14,4 @@ const version = {
     }
 };
 
-// Flag to indicate whether debug mode is enabled.
-// The mode is enabled when the version code includes a prerelease identifier but NOT for release candidates.
 const DEBUG = Boolean(version.prerelease && !version.prerelease.startsWith('rc'));

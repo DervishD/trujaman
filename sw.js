0f3bab0fd4af93718230a33f930f9981fc991a05
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2023-05-03 17:16:36 +0200

    Remove 'version.js' file
    The version code has been moved back into the service worker because in
    the end it was being used only there, while the rest of imports of the
    'version.js' file were for using the 'DEBUG' constant which is no longer
    used in the code.
    
    So, and since in fact the service worker can be actually considered as
    the app, version-wise, it has been moved back.
    
    For now, the structuring is kept.

--- sw.js
+++ sw.js
@@ -4,7 +4,17 @@ console.info('Service worker loaded');
 
 importScripts('version.js');  /* global version */
 
-
+// Follows Semantic Versioning 2.0.0 (https://semver.org/spec/v2.0.0.html).
+const version = {
+    'major': '0',
+    'minor': '1',
+    'patch': '0',
+    'prerelease': 'alpha',
+    'build': new Date().toISOString().split('T')[0].replaceAll('-', ''),
+    toString () {
+        return `${this.major}.${this.minor}.${this.patch}${this.prerelease && `-${this.prerelease}+${this.build}`}`;
+    }
+};
 const landingPage = '.';  // Maybe: "new URL(globalThis.registration.scope).pathname"???
 const cachePrefix = `trujaman@${globalThis.registration.scope}`;
 const currentCacheName = `${cachePrefix} v${version}`;

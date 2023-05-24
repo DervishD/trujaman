db316e8ee89a8955fa07f5728ce1fb799571b93c
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2023-05-24 16:43:28 +0200

    Modify message handling in web worker
    The switch/case that was being used was more than enough, an allowed for
    minor code optimizations. Nonetheless, it was not very scalable and it
    coupled the different handlers together since the message arguments were
    processed before getting into the different case branches.
    
    So, in order to make the handling more scalable and fully decouple the
    different handlers, the previously used solution (separate handlers) is
    used.

--- ww.js
+++ ww.js
@@ -21,99 +21,110 @@ globalThis.postReply = function postReply (reply, ...args) {
 
 
 globalThis.addEventListener('message', message => {
-    const {command, args} = message.data;
-    const [jobId] = args;  // Needed for most of the commands, so…
-    const job = globalThis.jobs.get(jobId);  // Idem…
-    console.debug(`Received command '${command}'`, args);
+    const {command, payload} = message.data;
+    console.debug(`Received command '${command}'`, payload);
 
-    switch (command) {
-    case 'registerFormats':
-        [globalThis.formats] = args;
-        break;
-    case 'slowModeToggle':
-        globalThis.slowModeEnabled = !globalThis.slowModeEnabled;
-        globalThis.postReply('slowModeStatus', globalThis.slowModeEnabled);
-        break;
-    case 'createJob': {
-        const [fileName] = args;
-        const newJob = {'file': fileName};
-        const newJobId = globalThis.currentJobId++;
-
-        // According to ECMA-262 which says that Number.MAX_SAFE_INTEGER equals
-        // (2^53)-1, and considering a scenario where 1000 jobs are added each
-        // millisecond, which is a bit optimistic, jobs could be added at that
-        // rate for a bit over 285 years for the test below to be true.
-        //
-        // So, it is safe to just silently fail here.
-        //
-        // Also, if the formats have not been registered, silently fail here.
-        // Because that should not happen, either…
-        if (!Number.isSafeInteger(newJobId) || !globalThis.formats) {
-            break;
-        }
-
-        newJob.reader = new FileReader();
-
-        newJob.reader.onprogress = event => {
-            const PERCENT_FACTOR = 100;
-            const percent = event.total ? Math.floor(PERCENT_FACTOR * event.loaded / event.total) : PERCENT_FACTOR;
-
-            if (globalThis.slowModeEnabled && globalThis.FILE_READING_DELAY_MILLISECONDS) {
-                const start = Date.now(); while (Date.now() - start < globalThis.FILE_READING_DELAY_MILLISECONDS);
-            }
-            globalThis.postReply('bytesRead', newJobId, percent);
-        };
-
-        newJob.reader.onerror = event => {
-            const error = {
-                'name': event.target.error.name,
-                'message': event.target.error.message,
-                'fileName': newJob.file.name
-            };
-            globalThis.postReply('fileReadError', newJobId, error);
-        };
-        newJob.reader.onload = event => {
-            const [marker] = new Uint8Array(event.target.result);
-            globalThis.postReply('fileReadOK', newJobId, marker);
-        };
-        newJob.reader.onabort = () => {
-            globalThis.postReply('jobCancelled', newJobId);
-        };
-
-        globalThis.jobs.set(newJobId, newJob);
-        globalThis.postReply('jobCreated', newJobId, newJob.file.name);
-        break;
-    }
-    case 'processJob':
-    case 'retryJob': {
-        const KIB_MULTIPLIER = 1024;
-        if (job.file.size > MAX_FILE_SIZE_MIB * KIB_MULTIPLIER * KIB_MULTIPLIER) {
-            const error = {
-                'name': 'FileTooLargeError',
-                'fileName': job.file.name
-            };
-            globalThis.postReply('fileReadError', jobId, error);
-        } else {
-            // The file is read using the HTML5 File API.
-            // Read the file as ArrayBuffer.
-            job.reader.readAsArrayBuffer(job.file);
-        }
-        break;
-    }
-    case 'cancelJob':
-        job.reader.abort();
-        break;
-    case 'deleteJob': {
-        job.reader.abort();
-        job.reader.onload = null;
-        job.reader.onerror = null;
-        job.reader.onabort = null;
-        globalThis.jobs.delete(args);
-
-        globalThis.postReply('jobDeleted', jobId);
-        break;
-    }
-    default:
+    const handler = `${command}Handler`;
+    if (handler in globalThis) {
+        globalThis[handler](payload);
+    } else {
         globalThis.postReply('commandNotFound', command);
     }
 });
+
+
+globalThis.createJobHandler = function createJobHandler (file) {
+    const job = {file, 'reader': null};
+    const jobId = globalThis.currentJobId++;
+
+    // According to ECMA-262 Number.MAX_SAFE_INTEGER is (2^53)-1. So, even in an
+    // scenario where 1000 jobs are added each millisecond, which is, in fact, a
+    // bit optimistic, jobs could be added at that rate for a bit over 285 years
+    // for the test below to be true.
+    //
+    // So, it is safe to just silently fail here.
+    //
+    // Also, if the formats have not been registered, silently fail here.
+    // Because that should not happen, either…
+    if (!Number.isSafeInteger(jobId) || !globalThis.formats) {
+        return;
+    }
+
+    job.reader = new FileReader();
+
+    job.reader.onprogress = event => {
+        const PERCENT_FACTOR = 100;
+        const percent = event.total ? Math.floor(PERCENT_FACTOR * event.loaded / event.total) : PERCENT_FACTOR;
+
+        if (globalThis.slowModeEnabled) {
+            const start = Date.now(); while (Date.now() - start < globalThis.FILE_READING_DELAY_MILLISECONDS);
+        }
+        globalThis.postReply('bytesRead', jobId, percent);
+    };
+
+    job.reader.onerror = event => {
+        const error = {
+            'name': event.target.error.name,
+            'message': event.target.error.message,
+            'fileName': job.file.name
+        };
+        globalThis.postReply('fileReadError', jobId, error);
+    };
+    job.reader.onload = event => {
+        const [marker] = new Uint8Array(event.target.result);
+        globalThis.postReply('fileReadOK', jobId, marker);
+    };
+    job.reader.onabort = () => {
+        globalThis.postReply('jobCancelled', jobId);
+    };
+
+    globalThis.jobs.set(jobId, job);
+    globalThis.postReply('jobCreated', jobId, job.file.name);
+};
+
+
+globalThis.registerFormatsHandler = function registerFormatsHandler (formats) {
+    globalThis.formats = formats;
+};
+
+
+globalThis.setSlowModeHandler = function setSlowModeHandler (state) {
+    globalThis.slowModeEnabled = state;
+    globalThis.postReply('slowModeState', globalThis.slowModeEnabled);
+};
+
+
+globalThis.processJobHandler = function processJobHandler (jobId) {
+    const job = globalThis.jobs.get(jobId);
+    const KIB_MULTIPLIER = 1024;
+
+    if (job.file.size > MAX_FILE_SIZE_MIB * KIB_MULTIPLIER * KIB_MULTIPLIER) {
+        const error = {
+            'name': 'FileTooLargeError',
+            'fileName': job.file.name
+        };
+        globalThis.postReply('fileReadError', jobId, error);
+    } else {
+        // The file is read using the HTML5 File API.
+        // Read the file as ArrayBuffer.
+        job.reader.readAsArrayBuffer(job.file);
+    }
+};
+globalThis.retryJobHandler = globalThis.processJobHandler;
+
+
+globalThis.cancelJobHandler = function cancelJobHandler (jobId) {
+    const job = globalThis.jobs.get(jobId);
+    job.reader.abort();
+};
+
+
+globalThis.deleteJobHandler = function deleteJobHandler (jobId) {
+    const job = globalThis.jobs.get(jobId);
+    job.reader.abort();
+    job.reader.onload = null;
+    job.reader.onerror = null;
+    job.reader.onabort = null;
+    globalThis.jobs.delete(jobId);
+    globalThis.postReply('jobDeleted', jobId);
+};

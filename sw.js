ea8cbe7c5ab4057a97807b4b7d40debc7a27c3fb
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2023-05-28 08:25:46 +0200

    Bump minor version after modularization

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Follows Semantic Versioning 2.0.0 (https://semver.org/spec/v2.0.0.html).
 const version = {
     'major': '0',
-    'minor': '1',
+    'minor': '2',
     'patch': '0',
     'prerelease': 'alpha',
     'build': new Date().toISOString().split('T')[0].replaceAll('-', ''),

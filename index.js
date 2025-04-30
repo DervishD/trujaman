DervishD/trujaman

commit a02bb83674ee29f22b235e79a4d2000d0ad610a9

Remove the 'processing' state for jobs
It is a very volatile state, intended only to show something during the
(very very short) time between a file is added to the app and it starts
being read. The message is barely readable because in the best case it
is only shown for a few milliseconds, so it does not make sense.

index.js
@@ -146,7 +146,6 @@ class UI {
 
 class Job {
     static states = {
-        processing: Symbol(MSG.JOB_STATE_PROCESSING),
         reading: Symbol(MSG.JOB_STATE_READING),
         processed: Symbol(MSG.JOB_STATE_PROCESSED),
         error: Symbol(MSG.JOB_STATE_ERROR),
@@ -337,10 +336,11 @@ class Presenter {
     }
 
     jobCreatedHandler ({jobId, fileName}) {
-        const newJob = new Job(jobId, fileName);
-        this.jobIds.set(jobId, newJob);
-        newJob.state = Job.states.processing;
-        this.webWorkerDo(commands.processJob, newJob.id);
+        const job = new Job(jobId, fileName);
+        this.jobIds.set(jobId, job);
+        job.progress = 0;
+        job.state = Job.states.reading;
+        this.webWorkerDo(commands.processJob, job.id);
     }
 
     jobDeletedHandler (jobId) {
@@ -352,7 +352,6 @@ class Presenter {
     bytesReadHandler ({jobId, percent}) {
         const job = this.jobIds.get(jobId);
         job.progress = percent;
-        job.state = Job.states.reading;
     }
 
     fileReadCompleteHandler ({jobId, contents}) {

constants.js
@@ -9,15 +9,11 @@ export const PERCENT_FACTOR = 100;
 const MAX_FILE_SIZE_MIB = 99;
 export const MAX_FILE_SIZE = MAX_FILE_SIZE_MIB * IEC_BINARY_PREFIX_MULTIPLIER * IEC_BINARY_PREFIX_MULTIPLIER;
 
-// For delaying file reading operations so the UI can be tested better, in "slow mode".
-export const FILE_READING_DELAY_MILLISECONDS = 500;
-
 // Selectors
 export const S_KEEP_ON_ERROR = '[data-keep-on-error]'
 export const S_DEFAULT_CONTROL = '[data-element="js-default-control"]'
 export const S_DROPZONE = '[data-element="js-dropzone"]'
 export const S_VERSION_TEXT = '[data-placeholder="js-version"]'
-export const S_SLOW_MODE_INDICATOR = '[data-placeholder="js-slow-mode-indicator"]'
 export const S_LOGO = '[data-element="js-logo"]'
 export const S_FILEPICKER = '[data-element="js-filepicker"]'
 export const S_FILEPICKER_INPUT = '[data-role="js-filepicker"]'

strings.js
@@ -22,7 +22,6 @@ export const WW_SENDING_REPLY = reply => `Sending reply '${reply}'`
 export const WW_RECEIVED_REPLY = reply => `Received reply '${reply}'`
 export const UNKNOWN_WW_REPLY = reply => `No se reconoce la respuesta del web worker «${reply}»`
 
-export const JOB_STATE_PROCESSING = 'Leyendo el fichero…'
 export const JOB_STATE_READING = 'Leyendo el fichero '
 export const JOB_STATE_PROCESSED = 'El fichero se leyó correctamente.'
 export const JOB_STATE_ERROR = 'Error: '

commit 2c988e1102901a2c250aab05376a994515d08e43

Fix bug introduced in commit a02bb83
The job state was not properly assigned when bytes were read. Because of
this, the percentage was not updated when reading the file.

index.js
@@ -348,6 +348,7 @@ class Presenter {
     bytesReadHandler ({jobId, percent}) {
         const job = this.jobRegistry.get(jobId);
         job.progress = percent;
+        job.state = Job.states.reading;
     }
 
     fileReadCompleteHandler ({jobId, contents}) {